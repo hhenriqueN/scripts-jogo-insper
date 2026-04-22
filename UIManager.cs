e8167fdbec5b1ef5d27a9114ca54c34bcc89f94c
Helio Henrique Navarro <heliohenriquenavarro@gmail.com>
2026-04-22 17:12:25 -0300

    Add UIManager to handle end game panel visibility
    Implement UIManager to show end game panel when game is over.

---
+++ UIManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    public GameObject endGamePanel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(GameManager.gameOver)
+        {
+            endGamePanel.SetActive(true);
+        }
+        
+    }
+}
