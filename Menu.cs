b35ab12875161cc0678172c8343939f6e5da300f
Helio Henrique Navarro <heliohenriquenavarro@gmail.com>
2026-04-22 19:06:35 -0300

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -3,14 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Este método vai no botão "JOGAR" do Menu
     public void IniciaJogo(){
         GameManager.Init();
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1); // Carrega a SampleScene
     }
 
+    // Este método vai no botão "MENU" da tela de Game Over
     public void MenuScreen(){
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0); // Volta para o MainMenu
     }
 
+    // DICA: Método para o botão "REINICIAR" na tela de Game Over
+    public void ReiniciarJogo(){
+        GameManager.Init();
+        SceneManager.LoadScene(1); // Carrega a fase de novo
+    }
 }
